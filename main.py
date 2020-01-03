5430d39f80efe026aca26483576f9fceb5aada95
Adam Oakes <51829203+AdamOakes@users.noreply.github.com>
2020-01-03 21:16:55 +0000

    Inital code

--- main.py
+++ main.py
@@ -1,1 +1,85 @@
+import math, random
 
+class Randomiser:
+        
+    def __init__(self): 
+      
+    def mixer(int passLength):
+        # this would work but need to give user option of which char types to use, so need to select them individually.
+        upperVal = ''
+        lowerVal = ''
+        symVal = ''
+        numVal = '' 
+        
+            
+        if(Interface.upperVal == true):
+            upperVal = 'string.ascii_lowercase'
+        if(Interface.lowerVal == true):
+            lowerVal = 'string.ascii_uppercase'
+        if(Interface.symVal == true):
+            symVal = 'string.digits'
+        if(Interface.numVal == true):
+            numVal = 'string.punctuation'
+        
+        # not sure what this will do if one of these options isn't selected.
+        password_characters = upperVal + lowerVal + symVal + numVal
+        return ''.join(random.choice(password_characters) for i in  range(passLength))   
+      
+ 
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
+
+root.distroy()
+
+class Generator:
+    
+    def __init__(self):
+
+    def getLen():
+        return len = Interface.lengthVal.get()
+        
+    def getUpper():
+        upper = Interface.upperVal.get()
+        if(upper == true):
+            return true
+        else return false
+        
+    
+    def getLower():
+        lower = Interface.lowerVal.get()
+        if(lower == true):
+            return true
+        else return false
+
+    def getSym():
+        sym = Interface.symVal.get()
+            if(sym == true):
+                return true
+            else return false
+
+    def getNum():
+        num = Interface.numVal.get()
+        if(num == true):
+            return true
+        else return false
